d5bf565d281f3016efbdcca44b7634523f3469f4
Becca Mason <beccamason28@gmail.com>
2021-06-18 11:43:35 +0100

    Fixed typo of PORT

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use("/", indexRouter);
 app.use("/weather", weatherRouter);
 app.use("*", errRouter);
 
-app.listen(process.env.port || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("App is online")
 });
